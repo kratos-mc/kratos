900ac5873a599f059fd6a3dfddcd68329ec93449
Player Nguyên <daudaua3782@gmail.com>
2023-04-03 15:25:42 +0700

    feat: a ipc listener to open a dev tool

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -2,7 +2,24 @@ import { ipcMain } from "electron/main";
 import { BrowserWindowManager } from "./window";
 
 export function loadIpcListener(browserManager: BrowserWindowManager) {
-  ipcMain.on("util:open-dev-tool", () => {
-    browserManager.getBrowserWindow("dev").show();
+  /**
+   *
+   */
+  ipcMain.on("util:open-dev-tool", (e, options) => {
+    const devWindow = browserManager.getBrowserWindow("dev");
+    if (devWindow === undefined) {
+    }
+
+    devWindow.show();
+    // Neither width nor height is an integer
+    if (options) {
+      options.width !== undefined ||
+        (options.height !== undefined &&
+          devWindow.setSize(
+            options.width ?? 300,
+            options.height ?? 400,
+            false
+          ));
+    }
   });
 }
