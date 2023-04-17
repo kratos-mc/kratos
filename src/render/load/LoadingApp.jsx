8de57fc91a7eb7b2b884a76e2e9830fe4760f49c
Player Nguyên <daudaua3782@gmail.com>
2023-04-17 17:33:38 +0700

    feat: add decoration for LoadingApp

--- src/render/load/LoadingApp.jsx
+++ src/render/load/LoadingApp.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("app")).render(
-  <div>
-    <div>Loading</div>
-  </div>
-);
+import "./../import-tailwind.css";
+import "./loading.css"
+export default function LoadingApp() {
+  return (
+    <div className="loading-wrapper dark:bg-neutral-700 bg-neutral-100 w-full h-full top-0 left-0 fixed flex flex-col items-center justify-center gap-2">
+      <div className="loading-text">
+        <b>Initializing </b>
+      </div>
+      <div className="loading-progress-bar h-2 w-2/3 rounded bg-neutral-900 px-1 py-1 animate-pulse"></div>
+    </div>
+  );
+}
