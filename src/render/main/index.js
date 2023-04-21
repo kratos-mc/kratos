8d9a802bc71449bb69ce1d60def70bc91dbc05ad
Player Nguyên <daudaua3782@gmail.com>
2023-04-21 10:55:12 +0700

    feat: add redux

--- src/render/main/index.js
+++ src/render/main/index.js
@@ -1,7 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import { store } from "./stores/RenderStore";
+import { Provider } from "react-redux";
+
 const app = document.getElementById("app");
 const root = createRoot(app);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
