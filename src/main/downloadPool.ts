3760dfaf9bfedc5f89d24f309fc5b2566296ded8
Player Nguyên <daudaua3782@gmail.com>
2023-04-24 12:58:06 +0700

    fix: using Indicator to update download status

--- src/main/downloadPool.ts
+++ src/main/downloadPool.ts
@@ -3,6 +3,8 @@ import { PromisePool } from "@supercharge/promise-pool";
 import { logger } from "./logger/logger";
 import { BrowserWindow } from "electron";
 import { IpcDictionary } from "./ipc";
+import { getDownloadIndicator } from "./app";
+import { indicator } from "./indicator/indicator";
 
 export class DownloadPool {
   private downloadProcessPool: PromisePool<
@@ -23,18 +25,24 @@ export class DownloadPool {
 
   public async downloadAll(window?: BrowserWindow) {
     const stackProcess = [...this.processes];
-
+    const downloadLength = stackProcess.length;
+    let currentProcessedItem: number = 0;
     if (window !== undefined) {
-      const downloadLength = stackProcess.length;
-
-      window.webContents.send(IpcDictionary.CREATE_DOWNLOAD, {
-        size: downloadLength,
-      });
+      // window.webContents.send(IpcDictionary.CREATE_DOWNLOAD, {
+      //   size: downloadLength,
+      // });
+      indicator.setProgressIndicator(
+        getDownloadIndicator().getId(),
+        0,
+        `Downloading ${downloadLength} items`,
+        ""
+      );
+      indicator.showIndicator(getDownloadIndicator().getId());
     }
 
     // stackProcess[0].startDownload();
     this.processes = [];
-    await this.downloadProcessPool
+    return this.downloadProcessPool
       .for(stackProcess)
       .withConcurrency(4)
       .process(async (downloadProcess, _index, _pool) => {
@@ -49,14 +57,20 @@ export class DownloadPool {
         );
 
         const _downloadInfo = await downloadProcess.startDownload();
-        if (window !== undefined) {
-          window.webContents.send(
-            IpcDictionary.PROGRESS_DOWNLOAD,
-            _downloadInfo
-          );
-        }
+        // console.log(`Downloaded [${_index}/${downloadLength}]`);
+        currentProcessedItem++;
+        indicator.setProgressIndicator(
+          getDownloadIndicator().getId(),
+          currentProcessedItem / downloadLength,
+          `Downloading ${downloadLength} items`,
+          `${downloadProcess.getDownloadInfo().destination}`
+        );
         // logger.info(`Successfully downloaded ${_downloadInfo.destination}`);
         return _downloadInfo;
+      })
+      .catch((err) => logger.error(err))
+      .finally(() => {
+        indicator.hideIndicator(getDownloadIndicator().getId());
       });
   }
 
